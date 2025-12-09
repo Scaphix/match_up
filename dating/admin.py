d1447583546aab2153ee7c97a6466319f80fcc55
Scaphix <chahinez.hoelzenbein@gmail.com>
2025-12-09 21:05:21 +0100

    Integrate Summernote for Profile admin, enabling rich text editing for bio and interests fields.

--- dating/admin.py
+++ dating/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import Profile
+from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
+class ProfileAdmin(SummernoteModelAdmin):
     list_display = ('user', 'age', 'gender', 'location', 'bio', 'interests', 'photo')
     list_filter = ('age', 'gender', 'location')
     search_fields = ('user__username', 'user__email')
+    summernote_fields = ('bio', 'interests')
