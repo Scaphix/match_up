3dab9ec419138df416e9782f968217e761f78bc0
Scaphix <chahinez.hoelzenbein@gmail.com>
2025-12-09 16:21:32 +0100

    Register Profile model in admin

--- match_up/settings.py
+++ match_up/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
@@ -102,6 +102,11 @@ else:
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
